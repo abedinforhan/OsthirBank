38e8019a82a80f9f06ad52a8be12532e4fcdbf57
osthirfarhan <farhanengn@gmail.com>
2020-07-22 02:21:43 +0600

    calculation part remaining

---
+++ script.js
@@ -0,0 +1,22 @@
+// const login=document.getElementById("loginBtn");
+// login.addEventListener("click",function(){
+//   document.getElementById("loginContainer").style.display="none";
+//   document.getElementById('transaction-area').style.display="block";
+  
+
+// });
+
+
+const deposit=document.getElementById("addDeposit");
+deposit.addEventListener("click",function(){
+  let depositAmount=parseFloat(document.getElementById("depositValue").value);
+  console.log(typeof(depositAmount));
+});
+
+const withdraw=document.getElementById("addWithdraw");
+withdraw.addEventListener("click",function(){
+  let withdrawAmount=parseFloat(document.getElementById("withdrawValue").value);
+  console.log(typeof(withdrawAmount));
+});
+
+  
