ab0a27e161aa87c4ef43cd8ef90edfb880032413
osthirfarhan <farhanengn@gmail.com>
2020-07-22 18:47:20 +0600

    finally done!!! yeeeh!!

--- script.js
+++ script.js
@@ -1,22 +1,91 @@
 const login=document.getElementById("loginBtn");
+   
 login.addEventListener("click",function(){
-  document.getElementById("loginContainer").style.display="none";
-  document.getElementById('transaction-area').style.display="block";
+      document.getElementById("loginContainer").style.display="none";
+      document.getElementById('transaction-area').style.display="block";
+  });
+
+//Deposit Event handler
+const deposit=document.getElementById("addDeposit");
+
+deposit.addEventListener("click",function(){
+   
+    let depositInputId=document.getElementById('depositInput');
+    let balanceInputId=document.getElementById('balanceInput');
+    
+    let depositAdded=depositInputId.value;
+    
+    document.getElementById('depositGrab').innerText=modifyAmount('depositGrab',depositAdded);
+    document.getElementById('balanceGrab').innerText=modifyAmount('balanceGrab',depositAdded);
+
+    depositInputId.value="";
+   });
+
+   //withdraw Handler
+  const withdraw=document.getElementById("addWithdraw");
+  
+    withdraw.addEventListener("click",function(){
+
+      let withdrawInputId=document.getElementById("withdrawInput");
+
+      let withdrawAdded=withdrawInputId.value;
+    
+      document.getElementById('withdrawGrab').innerText=modifyAmount('withdrawGrab',withdrawAdded);
+      document.getElementById('balanceGrab').innerText=modifyAmount("balanceGrab",-1*withdrawAdded);
+
+      withdrawInputId.value="";
+     
+    });
+
+
+   const modifyAmount=(grabId,addAmount)=>{
+      let  currentAmountId=document.getElementById(grabId);
+      
+      let  currentAmount= currentAmountId.innerText;
+      // console.log(currentAmount);
+      
+      let   total=parseFloat(currentAmount)+parseFloat(addAmount);
+        
+      return total;
+
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
 
-});
 
 
-const deposit=document.getElementById("addDeposit");
-deposit.addEventListener("click",function(){
-  let depositAmount=parseFloat(document.getElementById("depositValue").value);
-  console.log(typeof(depositAmount));
-});
 
-const withdraw=document.getElementById("addWithdraw");
-withdraw.addEventListener("click",function(){
-  let withdrawAmount=parseFloat(document.getElementById("withdrawValue").value);
-  console.log(typeof(withdrawAmount));
-});
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+   
+
+
+
+   
+
+
+
+
+
+
+
 
   
